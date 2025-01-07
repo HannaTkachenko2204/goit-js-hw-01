53edd2eb27f45a8ed30bcde7378cfdf9ab7556f5
Анна <anna.kuvakina91@gmail.com>
2025-01-07 15:44:03 +0200

    draft

---
+++ js/task-4.js
@@ -0,0 +1,20 @@
+function makeMessage(username) {
+	console.log(`Hello ${username}`);
+}
+
+makeMessage("Jacob");
+
+// function multiply(x, y, z) {
+//     console.log(`Result: ${x * y * z}`);
+//   }
+  
+  console.log("Log before multiply execution");
+  multiply(2, 3, 5); // "Result: 30"
+  console.log("Log after multiply execution");
+
+  function multiply(x, y, z) {
+	const product = x * y * z;
+}
+
+const result = multiply(2, 3, 5);
+console.log(result);
